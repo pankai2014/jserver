fd9a6bcbedfeba7ee50ecfed6bbc445695edaa92
will <530911044@qq.com>
2016-08-07 17:09:52 +0800

    complish fastcgi client

--- src/org/kaipan/www/socket/fastcgi/Client.java
+++ src/org/kaipan/www/socket/fastcgi/Client.java
@@ -18,24 +18,24 @@ import org.kaipan.www.socket.core.MessageBuffer;
 
 public class Client
 {
-    public final static byte VERSION           = 1;
+    public final static int VERSION           = 1;
     
-    public final static byte BEGIN_REQUEST     = 1;
-    public final static byte ABORT_REQUEST     = 2;
-    public final static byte END_REQUEST       = 3;
-    public final static byte PARAMS            = 4;
-    public final static byte STDIN             = 5;
-    public final static byte STDOUT            = 6;
-    public final static byte STDERR            = 7;
-    public final static byte DATA              = 8;
-    public final static byte GET_VALUES        = 9;
-    public final static byte GET_VALUES_RESULT = 10;
-    public final static byte UNKNOWN           = 11;
-    public final static byte MAX               = UNKNOWN; 
+    public final static int BEGIN_REQUEST     = 1;
+    public final static int ABORT_REQUEST     = 2;
+    public final static int END_REQUEST       = 3;
+    public final static int PARAMS            = 4;
+    public final static int STDIN             = 5;
+    public final static int STDOUT            = 6;
+    public final static int STDERR            = 7;
+    public final static int DATA              = 8;
+    public final static int GET_VALUES        = 9;
+    public final static int GET_VALUES_RESULT = 10;
+    public final static int UNKNOWN           = 11;
+    public final static int MAX               = UNKNOWN; 
    
-    public final static byte ROLE_RESPONDER    = 1;
-    public final static byte ROLE_AUTHORIZER   = 2;
-    public final static byte ROLE_FILTER       = 3;
+    public final static int ROLE_RESPONDER    = 1;
+    public final static int ROLE_AUTHORIZER   = 2;
+    public final static int ROLE_FILTER       = 3;
     
     public final static int HEADER_LEN         = 8;
     
@@ -45,7 +45,9 @@ public class Client
     private int connectTimeOut   = 5000;
     private int readWriteTimeOut = 5000;
     
-    private ByteBuffer bytes = ByteBuffer.allocate(65535);
+    private byte[]	   readBytes		= new byte[65535];	
+    private ByteBuffer readBytesBuffer  = ByteBuffer.wrap(readBytes);
+    private ByteBuffer writeBytesBuffer = ByteBuffer.allocate(65535);
     
     private Socket  client  = null;
     private Message message = null;
@@ -72,6 +74,7 @@ public class Client
         SocketAddress address = new InetSocketAddress(host, port);
         try {
             client.connect(address, connectTimeOut);
+            client.setSoTimeout(readWriteTimeOut);
         } 
         catch (IOException e) {
             // TODO Auto-generated catch block
@@ -94,20 +97,28 @@ public class Client
         connect();
         
         int requestId = random();
+        System.out.println("requestId: " + requestId);
         
         builStartPacket(requestId);
         
-        buildParamsPacket(requestId, params);
+        if ( params != null ) {
+        	buildParamsPacket(requestId, params);
+        }
+        
         buildParamsPacket(requestId, null);
         
-        buildStdinPacket(requestId, stdin);
+        if ( stdin != null ) {
+        	buildStdinPacket(requestId, stdin);
+        }
+        
         buildStdinPacket(requestId, null);
         
         try {
             OutputStream outStream      = client.getOutputStream();
             BufferedOutputStream outBuf = new BufferedOutputStream(outStream);
             
-            outBuf.write(message.sharedArray, 0, message.length);
+            outBuf.write(message.sharedArray, message.offset, message.length);
+            outBuf.flush();
         } 
         catch (IOException e) {
             // TODO Auto-generated catch block
@@ -117,114 +128,174 @@ public class Client
         waitForResponse(requestId);
     }
     
-    public void waitForResponse(int requestId) 
+    private void addHeader(int requestId, int type, int clen) 
     {
-        try {
-            InputStream inStream        = client.getInputStream();
-            BufferedInputStream  inBuf  = new BufferedInputStream(inStream);
-            
-            int ready;
-            while ( (ready = inBuf.read()) != -1 ) {
-                System.out.println(ready);
-            }
-        } 
-        catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+    	writeBytesBuffer.put( (byte) ((VERSION) & 0xFF) );             /* version */
+    	writeBytesBuffer.put( (byte) ((type)    & 0xFF) );             /* type */
+        
+    	writeBytesBuffer.put( (byte) ((requestId >> 8) & 0xFF) );      /* requestIdB1, (& 0xFF) unsigned character!!! */
+    	writeBytesBuffer.put( (byte) ((requestId)      & 0xFF) );      /* requestIdB0 */
+        
+    	writeBytesBuffer.put( (byte) ((clen >> 8) & 0xFF) );           /* contentLengthB1 */
+    	writeBytesBuffer.put( (byte) ((clen)      & 0xFF) );           /* contentLengthB0 */
+        
+    	writeBytesBuffer.put( (byte) (0 & 0x00) );                     /* paddingLength */
+    	writeBytesBuffer.put( (byte) (0 & 0x00) );                     /* reserved */
     }
     
-    public void addHeader(int requestId, byte type, int clen) 
-    {
-        bytes.put(VERSION);                                 /* version */
-        bytes.put(type);                                    /* type */
-        
-        bytes.put( (byte) ((requestId >> 8) & 0xFF) );      /* requestIdB1 */
-        bytes.put( (byte) ((requestId)      & 0xFF) );      /* requestIdB0 */
-        
-        bytes.put( (byte) ((clen >> 8) & 0xFF) );           /* contentLengthB1 */
-        bytes.put( (byte) ((clen)      & 0xFF) );           /* contentLengthB0 */
-        
-        bytes.put( (byte) (0 & 0x00) );                     /* paddingLength */
-        bytes.put( (byte) (0 & 0x00) );                     /* reserved */
-    }
-    
-    public void builStartPacket(int requestId) 
+    private void builStartPacket(int requestId) 
     {
         addHeader(requestId, BEGIN_REQUEST, HEADER_LEN);
         
         int loc = 0;
         
-        bytes.put( (byte) (0 & 0x00) );         loc++;
-        bytes.put(ROLE_RESPONDER);              loc++;
-        bytes.put((byte) (keepAlive ? 1 : 0));  loc++;
+        writeBytesBuffer.put( (byte) (0 & 0x00) );         		    loc++;
+        writeBytesBuffer.put( (byte) (ROLE_RESPONDER 	  & 0xFF)); loc++;
+        writeBytesBuffer.put( (byte) ((keepAlive ? 1 : 0) & 0xFF)); loc++;
         
         for ( int i =  loc; i < HEADER_LEN; i++ ) {
-            bytes.put( (byte) (0 & 0x00) );
+        	writeBytesBuffer.put( (byte) (0 & 0x00) );
         }
         
-        bytes.flip();
-        message.writeToMessage(bytes);
+        writeBytesBuffer.flip();
+        message.writeToMessage(writeBytesBuffer);
         
-        bytes.clear();
+        writeBytesBuffer.clear();
     }
     
-    public void buildParamsPacket(int requestId, Map<String, String> params) 
+    private void buildParamsPacket(int requestId, Map<String, String> params) 
     {
-        for ( Map.Entry<String, String> entry : params.entrySet() ) {
-            buildNvpair(this.bytes, entry.getKey(), entry.getValue());
-        }
-        
-        int clen = 0;
-        if ( params != null ) {
-            clen = bytes.position() + 1;
-        }
-        
-        addHeader(requestId, PARAMS, clen);
-        
-        bytes.flip();
-        message.writeToMessage(bytes.array(), clen, bytes.remaining() - clen);
-        message.writeToMessage(bytes.array(), 0, clen);
-        
-        bytes.clear();
+    	if ( params != null ) {
+    	     for ( Map.Entry<String, String> entry : params.entrySet() ) {
+    	        buildNvpair(entry.getKey(), entry.getValue());
+    	     }
+    	     
+    	}
+    	
+    	readBytesBuffer.flip();
+    	
+    	addHeader(requestId, PARAMS, readBytesBuffer.remaining());
+    	
+      	writeBytesBuffer.flip();
+    	
+    	message.writeToMessage(writeBytesBuffer);
+    	message.writeToMessage(readBytesBuffer);
+    	
+    	readBytesBuffer.clear();
+    	writeBytesBuffer.clear();
     }
     
-    public void buildStdinPacket(int requestId, byte[] stdin) 
+    private void buildStdinPacket(int requestId, byte[] stdin) 
     {
-        addHeader(requestId, STDIN, stdin.length);
+    	int clen = 0;
+        if ( stdin != null ) {
+        	writeBytesBuffer.put(stdin);
+        	
+        	clen = stdin.length;
+        }
         
-        bytes.put(stdin);
+        addHeader(requestId, STDIN, clen);
         
-        bytes.flip();
-        message.writeToMessage(bytes);
+        writeBytesBuffer.flip();
+        message.writeToMessage(writeBytesBuffer);
         
-        bytes.clear();
+        writeBytesBuffer.clear();
     }
     
-    public void buildNvpair(ByteBuffer bytes, String name, String value) 
+    private void buildNvpair(String name, String value) 
     {
         int nlen = name.length();
         int vlen = value.length();
         
         if ( nlen < 128 ) {
-            bytes.put( (byte) nlen );
+        	readBytesBuffer.put( (byte) (nlen & 0xFF) );
         }
         else {
-            bytes.put( (byte) ((nlen >> 24) | 0x80) );     /* nameLengthB3 */
-            bytes.put( (byte) ((nlen >> 16) & 0xFF) );     /* nameLengthB2 */
-            bytes.put( (byte) ((nlen >> 8)  & 0xFF) );     /* nameLengthB1 */
-            bytes.put( (byte) ((nlen)       & 0xFF) );     /* nameLengthB0 */
+        	readBytesBuffer.put( (byte) ((nlen >> 24) | 0x80) );     /* nameLengthB3 */
+        	readBytesBuffer.put( (byte) ((nlen >> 16) & 0xFF) );     /* nameLengthB2 */
+        	readBytesBuffer.put( (byte) ((nlen >> 8)  & 0xFF) );     /* nameLengthB1 */
+        	readBytesBuffer.put( (byte) ((nlen)       & 0xFF) );     /* nameLengthB0 */
         }
         
         if ( vlen < 128 ) {
-            bytes.put( (byte) nlen );
+        	readBytesBuffer.put( (byte) (vlen & 0xFF) );
         }
         else {
-            bytes.put( (byte) ((vlen >> 24) | 0x80) );     /* valueLengthB3 */
-            bytes.put( (byte) ((vlen >> 16) & 0xFF) );     /* valueLengthB2 */
-            bytes.put( (byte) ((vlen >> 8)  & 0xFF) );     /* valueLengthB1 */
-            bytes.put( (byte) ((vlen)       & 0xFF) );     /* valueLengthB0 */
+        	readBytesBuffer.put( (byte) ((vlen >> 24) | 0x80) );     /* valueLengthB3 */
+        	readBytesBuffer.put( (byte) ((vlen >> 16) & 0xFF) );     /* valueLengthB2 */
+        	readBytesBuffer.put( (byte) ((vlen >> 8)  & 0xFF) );     /* valueLengthB1 */
+        	readBytesBuffer.put( (byte) ((vlen)       & 0xFF) );     /* valueLengthB0 */
         }
+        
+        readBytesBuffer.put(name.getBytes());
+        readBytesBuffer.put(value.getBytes());
+    }
+    
+    private int readPacket()
+    {
+    	int clen   = 0;
+    	int length = 0;
+    	
+    	try {
+            InputStream inStream       = client.getInputStream();
+            BufferedInputStream  inBuf = new BufferedInputStream(inStream);
+            
+            while ( (clen = inBuf.read(readBytes)) != -1 ) {
+            	length += clen;
+            }
+            
+            client.close();
+        } 
+        catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    	
+    	return length;
+    }
+    
+    public void waitForResponse(int requestId) 
+    {
+        int length = this.readPacket();
+        
+        for ( int i = 0; i < length; ) {
+        	int responseId     = ((readBytes[i + 2] << 8) & 0xFFFF) + ((readBytes[i + 3]) & 0xFF); 	// byte will be forced to transfer to int when calculating(!!!)
+        	
+        	if ( responseId != requestId ) continue;
+        	
+        	//int version 	   = ((readBytes[i] & 0xFF));
+        	int type    	   = ((readBytes[i + 1] & 0xFF));
+
+        	int contentLength  = ((readBytes[i + 4] << 8) & 0xFFFF) + ((readBytes[i + 5]) & 0xFF); 	// bytes[2] & 0xFF, bytes[4] & 0xFF, (& 0xFF or & oxFFFF) must be set in last
+        	int paddingLength  = ((readBytes[i + 6] & 0xFF));
+        	//int reserved 	   = ((readBytes[i + 7] & 0xFF));
+        	
+        	switch ( type ) {
+        		case STDOUT:
+        			writeBytesBuffer.put(readBytes, i + HEADER_LEN, contentLength);
+        			break;
+        		case GET_VALUES_RESULT:	
+        			break;
+        		case STDERR:
+        			break;
+        		case END_REQUEST:
+        		case UNKNOWN:	
+        			break;
+        	}
+        	
+        	i += HEADER_LEN + contentLength + paddingLength;
+        }
+        
+        //System.out.println(writeBytesBuffer);
+        writeBytesBuffer.flip();
+        System.out.println(new String(writeBytesBuffer.array(), 0, writeBytesBuffer.remaining()));
+        
+        
+        readBytesBuffer.clear();
+        writeBytesBuffer.clear();
+        
+        readBytesBuffer  = null;
+        writeBytesBuffer = null;
     }
     
     public static void main(String[] args) 
@@ -239,14 +310,14 @@ public class Client
         params.put("GATEWAY_INTERFACE", "FastCGI/1.0");
         params.put("REQUEST_METHOD", "GET");
         
-        params.put("SCRIPT_FILENAME", "/");
+        params.put("SCRIPT_FILENAME", "/home/will/Develop/projects/app/www/index.php");
         
         params.put("SCRIPT_NAME", "/index.php");
         params.put("QUERY_STRING", "");
         
-        params.put("REQUEST_URI", "http://localhost:9000/index.php");
+        params.put("REQUEST_URI", "/index.php");
         params.put("DOCUMENT_URI", "/index.php");
-        params.put("SERVER_SOFTWARE", "FastCGI/1.0");
+        params.put("SERVER_SOFTWARE", "php/fcgiclient");
         
         params.put("REMOTE_ADDR", "127.0.0.1");
         params.put("REMOTE_PORT", "9000");
@@ -254,11 +325,11 @@ public class Client
         params.put("SERVER_ADDR", "127.0.0.1");
         params.put("SERVER_PORT", "80");
         
-        params.put("SERVER_NAME", "");
+        params.put("SERVER_NAME", "will-All-Series");
         params.put("SERVER_PROTOCOL", "HTTP/1.1");
         
         params.put("CONTENT_TYPE", "");
-        params.put("CONTENT_LENGTH", "");
+        params.put("CONTENT_LENGTH", "0");
         
         client.request(params, null);
     }
